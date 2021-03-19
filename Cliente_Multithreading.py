b71fb8006c8411b133e50d4ea117b000407f5ca9
Nicole Bahamon <n.bahamon@uniandes.edu.co>
2021-03-19 10:07:02 -0500

    Hash funciona

--- Cliente_Multithreading.py
+++ Cliente_Multithreading.py
@@ -1,4 +1,5 @@
 import socket
+import hashlib
 
 ClientSocket=socket.socket()
 host='127.0.0.1'
@@ -10,6 +11,7 @@ try:
     ClientSocket.connect((host,port))
 except socket.error as e:
     print(str(e))
+##
 
 # while True:
 #     estado='Listo para recibir'
@@ -35,12 +37,42 @@ with open('received_file', 'wb') as f:
         print('receiving data...')
         data = ClientSocket.recv(1024)
         print('data=%s', (data))
-        if not data:
+        if 'Thank you for connecting' in data.decode():
+            hash_recibido=ClientSocket.recv(1024).decode()
+        elif not data:
             break
         # write data to a file
-        f.write(data)
+        else:
+            f.write(data)
 
 f.close()
 print('Successfully get the file')
+print(hash_recibido)
 ClientSocket.close()
 print('connection closed')
+
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+message = hash_file('received_file')
+if hash_recibido!=message:
+    print('Error, el archivo no mantiene su integridad')
+else:
+    print('Archivo recibido correctamente')
--- Servidor_Multithreading.py
+++ Servidor_Multithreading.py
@@ -1,6 +1,8 @@
 import socket
 import os
+import hashlib
 from _thread import *
+
 
 
 ServerSocket=socket.socket()
@@ -28,10 +30,31 @@ else:
     print('No se reconoce el archivo, se maneja el archivo de 100MB')
 ServerSocket.listen(5)
 print('Server listening....')
+##
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
 
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
+message = hash_file(Archivo)
+print(message)
 ##
 def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Servern'))
     while True:
         filename =Archivo  # In the same folder or path is this file running must the file you want to tranfser to be
         f = open(filename, 'rb')
@@ -45,8 +68,9 @@ def threaded_client(connection):
                 pass
         f.close()
 
-        print('Done sending')
+        print('Done sending \n')
         connection.send(str.encode('Thank you for connecting'))
+        connection.send(str.encode(message))
         connection.close()
 
 while True:

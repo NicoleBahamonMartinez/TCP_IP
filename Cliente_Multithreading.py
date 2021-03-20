78525c83c48b3158648a086a80534363d873cfe7
Nicole Bahamon <n.bahamon@uniandes.edu.co>
2021-03-20 17:41:19 -0500

    Log ya solo le faltan 2 cosas a cada uno

--- Cliente_Multithreading.py
+++ Cliente_Multithreading.py
@@ -1,7 +1,6 @@
 import socket
 import hashlib
 import os
-import time
 import logging
 from datetime import datetime
 
@@ -21,48 +20,7 @@ try:
     ClientSocket.connect((host,port))
 except socket.error as e:
     print(str(e))
-##
-if nombre_Archivo=='':
-    Estado='Listo para recibir'
-    ClientSocket.send(str.encode(Estado))
-else:
-    ClientSocket.send(str.encode('Recibiendo Archivo'))
-
-
-datos_iniciales = ClientSocket.recv(1024)
-datos_iniciales=datos_iniciales.decode()
-datos_iniciales=str(datos_iniciales)
-datos_iniciales=datos_iniciales.split('-')
-
-if not os.path.exists('ArchivosRecibidos'):
-
-    os.makedirs('ArchivosRecibidos')
-nombre_Archivo='ArchivosRecibidos/Cliente'+str(datos_iniciales[0])+'-Prueba-'+str(datos_iniciales[1])+'.txt'
-
-print(nombre_Archivo)
-
-
-with open(nombre_Archivo, 'wb') as f:
-    print('file opened')
-    while True:
-        print('receiving data...')
-        data = ClientSocket.recv(1024)
-        # print('data=%s', (data))
-        if 'Thank you for connecting' in data.decode():
-            hash_recibido=ClientSocket.recv(1024).decode()
-        elif not data:
-            print('Closing file')
-            break
-        # write data to a file
-        else:
-            f.write(data)
-
-f.close()
-print('Successfully get the file')
-print(hash_recibido)
-ClientSocket.close()
-print('connection closed')
-
+## Fúncion para hacer hashing del archivo
 def hash_file(filename):
    """"This function returns the SHA-1 hash
    of the file passed into it"""
@@ -82,10 +40,62 @@ def hash_file(filename):
 
    # return the hex representation of digest
    return h.hexdigest()
+## Estado actual del cliente
+if nombre_Archivo=='':
+    Estado='Listo para recibir'
+    ClientSocket.send(str.encode(Estado))
+else:
+    ClientSocket.send(str.encode('Recibiendo Archivo'))
+## Recibimiento de datos del servidor
+datos_iniciales = ClientSocket.recv(1024)
+datos_iniciales=datos_iniciales.decode()
+datos_iniciales=str(datos_iniciales)
+datos_iniciales=datos_iniciales.split('-')
+
+## Creación de path donde se guarda el archivo
+if not os.path.exists('ArchivosRecibidos'):
+
+    os.makedirs('ArchivosRecibidos')
+nombre_Archivo='ArchivosRecibidos/Cliente'+str(datos_iniciales[0])+'-Prueba-'+str(datos_iniciales[1])+'.txt'
+
+print(nombre_Archivo)
+# Logging
+logging.info('Nombre de archivo recibido :'+str(datos_iniciales[2]))
+logging.info('Soy el cliente '+str(datos_iniciales[0]+' de '+str(datos_iniciales[1])+' conexiones'))
+
+## Escritura de paquetes recibidos hacia archivo
+with open(nombre_Archivo, 'wb') as f:
+    print('file opened')
+    while True:
+        print('receiving data...')
+        data = ClientSocket.recv(1024)
+        # print('data=%s', (data))
+        if 'Thank you for connecting' in data.decode():
+            hash_recibido=ClientSocket.recv(1024).decode()
+            print(hash_recibido)
+            print('Recibe el hash')
+            print('Closing file')
+            break
+        # write data to a file
+        else:
+            f.write(data)
+
+f.close()
+print('Successfully get the file')
+tamanio_Archivo=os.stat(nombre_Archivo).st_size
+logging.info('Tamaño de archivo recibido: '+str(tamanio_Archivo)+' bytes')
+## Hashing del archivo
 print('Comienza el hashing')
 message = hash_file(nombre_Archivo)
 print('Termina el hashing')
 if hash_recibido!=message:
+    ClientSocket.send(str.encode('No se mantiene la integridad'))
     print('Error, el archivo no mantiene su integridad')
+    logging.info('Archivo recibido no exitosamente')
 else:
+    ClientSocket.send(str.encode('Se mantiene la integridad'))
     print('Archivo recibido correctamente')
+    logging.info('Archivo recibido exitosamente')
+ClientSocket.close()
+print('connection closed')
+
--- Servidor_Multithreading.py
+++ Servidor_Multithreading.py
@@ -1,5 +1,4 @@
 import socket
-import os
 import hashlib
 from _thread import *
 import time
@@ -38,8 +37,8 @@ else:
     Archivo_Tamaño=100
     print('No se reconoce el archivo, se maneja el archivo de 100MB')
 ServerSocket.listen(5)
-logging.debug('Nombre de archivo enviado'+Archivo)
-logging.debug('Tamaño de archivo enviado'+Archivo_Tamaño+'MB')
+logging.info('Nombre de archivo enviado: '+Archivo)
+logging.info('Tamaño de archivo enviado: '+Archivo_Tamaño+' MB')
 print('Server listening....')
 ##
 def hash_file(filename):
@@ -61,10 +60,16 @@ def hash_file(filename):
 
    # return the hex representation of digest
    return h.hexdigest()
+
 def enviar_hash(connection):
     message = hash_file(Archivo)
     print(message)
     connection.send(str.encode(message))
+    b=connection.recv(1024).decode('utf-8')
+    if 'No' in b:
+        logging.info('Transferencia no exitosa')
+    else:
+        logging.info('Transferencia exitosa')
 
 ##
 def threaded_client(connection):
@@ -88,12 +93,13 @@ def threaded_client(connection):
 
 
 
+
         connection.close()
 
 while True:
     Client,address=ServerSocket.accept()
     print('Connected to: '+address[0]+':'+str(address[1]))
-    Booleano=EstadoClientes==(Client.recv(2048).decode('utf-8'))
+    Booleano=EstadoClientes==(Client.recv(1024).decode('utf-8'))
     if Booleano:
         Connections+=1
         Clientes.append(Client)
@@ -102,15 +108,16 @@ while True:
     if Connections==NumeroConexiones:
         print('Se comienza a enviar el archivo')
         for i in range(NumeroConexiones):
-            Clientes[i].send(str.encode(str(i+1)+'-'+str(NumeroConexiones)))
+            Clientes[i].send(str.encode(str(i+1)+'-'+str(NumeroConexiones)+'-'+Archivo))
             print('Enviando número de thread y conexiones totales')
             time.sleep(1)
             start_new_thread(threaded_client,(Clientes[i],))
             ThreadCount+=1
+            logging.info('Cantidad de conexiones: ' + str(NumeroConexiones) + ' Número de Cliente ' + str(ThreadCount))
             print('Thread Number: '+str(ThreadCount))
         Clientes=[]
-        ThreadCount=0
         Connections=0
+        ThreadCount=0
 
 ServerSocket.close()
 
